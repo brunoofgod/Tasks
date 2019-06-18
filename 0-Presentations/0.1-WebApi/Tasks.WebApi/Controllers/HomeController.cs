df5581a54f5d3a694b4b5f5fe4ab464b9885a567
Bruno de Deus <bruno_gdd@hotmail.com>
2019-06-18 16:03:55 -0300

    Redirecionando para swagger

---
+++ 0-Presentations/0.1-WebApi/Tasks.WebApi/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Tasks.WebApi.Controllers
+{
+    [Route("/")]
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public RedirectResult Get()
+        {
+            return Redirect("/swagger");
+        }
+    }
+}
